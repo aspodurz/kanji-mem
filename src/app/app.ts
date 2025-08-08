e2b1708401393260016d4091bbec0cd9a33828f3
Antonio Talarico <g21anttalari@icc.crifnet.com>
2025-08-08 18:11:18 +0200

    first release

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,37 @@
-import { Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatButtonModule } from '@angular/material/button';
+import datas from './data/entries.json'
+import { Elements, Entry } from './data/entry';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [MatButtonModule,MatExpansionModule,RouterOutlet],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('kanji-mem');
+
+  data: Elements = (datas as Elements);
+  entry: Entry;
+  len: number;
+
+  constructor(){
+    this.len=this.data.elements.length;
+    this.entry=this.data.elements[this.getRandomInt(this.len)];
+  }
+
+  nextItem() {
+    this.entry=this.data.elements[this.getRandomInt(this.len)];
+  }
+
+  getRandomInt(max: number) {
+    return Math.floor(Math.random() * max);
+  }
+
+
 }
---
+++ src/app/data/entry.ts
@@ -0,0 +1,11 @@
+export class Elements{
+    elements: Array<Entry>=new Array;
+}
+
+export class Entry{
+    id: number=-1;
+    kanji: String='';
+    pronounces: Array<String>=new Array;
+    tranlsations: Array<String>=new Array;
+}
+
