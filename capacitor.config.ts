e97be65e7cc7fa0a7cb4667af60053c1c34b75d1
Antonio Talarico <g21anttalari@icc.crifnet.com>
2025-08-08 15:13:48 +0200

    init project

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'net.spx.app',
+  appName: 'kanji-mem',
+  webDir: 'dist/kanji-mem/browser'
+};
+
+export default config;
