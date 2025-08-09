dbd0348d8010696d4e0b8205bf3fe2059ce468e5
Antonio Talarico <g21anttalari@icc.crifnet.com>
2025-08-09 17:36:03 +0200

    update data

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,37 +1,53 @@
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FormControl, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatButtonModule } from '@angular/material/button';
-import datas from './data/entries.json'
+import {MatInputModule} from '@angular/material/input';
+import datas from './data/entries.json';
 import { Elements, Entry } from './data/entry';
+
+
 
 
 @Component({
   selector: 'app-root',
-  imports: [MatButtonModule,MatExpansionModule,RouterOutlet],
+  imports: [MatButtonModule,MatExpansionModule,MatInputModule,ReactiveFormsModule,RouterOutlet],
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('kanji-mem');
-
+  //245=>325 last checked id
+  protected readonly defaultMaxId: number = 245+1;
   data: Elements = (datas as Elements);
+  maxIdFormControl = new FormControl(this.defaultMaxId, [Validators.required, Validators.min(0), Validators.max(this.data.elements.length), Validators.pattern('^[\\d]+\\.{0,1}[\\d]*$')]);
   entry: Entry;
-  len: number;
-
+  
   constructor(){
-    this.len=this.data.elements.length;
-    this.entry=this.data.elements[this.getRandomInt(this.len)];
+    this.entry=this.data.elements[this.getRandomInt(this.getMaxId())];
   }
 
   nextItem() {
-    this.entry=this.data.elements[this.getRandomInt(this.len)];
+    this.entry=this.data.elements[this.getRandomInt(this.getMaxId())];
   }
 
-  getRandomInt(max: number) {
+  getMaxId(): number {
+    let len:number;
+    if(this.maxIdFormControl.value!=null){
+      len=this.maxIdFormControl.value;
+    }else{
+      len=this.defaultMaxId;
+    }
+    return len;
+  }
+
+  getRandomInt(max: number) : number {
     return Math.floor(Math.random() * max);
   }
+
 
 
 }
--- src/app/data/entry.ts
+++ src/app/data/entry.ts
@@ -4,8 +4,9 @@ export class Elements{
 
 export class Entry{
     id: number=-1;
+    url?: String='';
     kanji: String='';
     pronounces: Array<String>=new Array;
-    tranlsations: Array<String>=new Array;
+    translations: Array<String>=new Array;
 }
 
